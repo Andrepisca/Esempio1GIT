4f2d5f3edd7405a9d1e51601eb2d8451914bf265
Andrepisca <115340124+Andrepisca@users.noreply.github.com>
2022-10-08 09:41:00 +0200

    versione 2: aggiunti commenti

--- 08-10-2022/MainWindow.xaml.cs
+++ 08-10-2022/MainWindow.xaml.cs
@@ -24,17 +24,17 @@ namespace _08_10_2022
         {
             InitializeComponent();
         }
-
+        //bottone che calcola la media
         private void btnMedia_Click(object sender, RoutedEventArgs e)
         {
             double[] v = new double[10];
             Random rnd = new Random();
-
+            //ciclo per caricare il vettoredi numeri casuali
             for(int i = 0; i < 10; i++)
                 v[i] = rnd.Next(0,100);
            
             double somma =0;
-
+            //calcolo media
             for (int i = 0; i < 10; i++)
                 somma += v[i];
 
